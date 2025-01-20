c763a36d9b160afa8a1ed19b1b07bb75d903ed3c
Lucio Paiva <luciopaiva@gmail.com>
2025-01-20 17:13:59 +0000

    Fix visible points on the back

--- src/particles-material.js
+++ src/particles-material.js
@@ -18,14 +18,11 @@ export default class ParticlesMaterial extends THREE.PointsMaterial {
         shader.vertexShader = shader.vertexShader.replace(
             `gl_PointSize = size;`,
             `
-                    vec3 vNormal = normalize( normalMatrix * normal );
-                    vec3 vDir = vec3(0, 0, 1);
-                    vVisible = step( 0., dot( vDir, vNormal ) );
-                    
+                    vec3 vNormal = normalMatrix * normal;
+                    vVisible = step( 0., dot( -normalize(mvPosition.xyz), normalize(vNormal) ) );
+                   
                     gl_PointSize = size;
             `);
-        console.info("-".repeat(80));
-        console.log(shader.fragmentShader);
         shader.fragmentShader = `
                 varying float vVisible;
             ` + shader.fragmentShader;
@@ -35,6 +32,6 @@ export default class ParticlesMaterial extends THREE.PointsMaterial {
                 if ( floor(vVisible + 0.1) == 0.0 ) discard;
                 #include <clipping_planes_fragment>
             `);
-        console.log(shader.fragmentShader);
+
     }
 }
